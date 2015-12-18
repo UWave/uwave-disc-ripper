161ef3107b4cc8141400020641144abfdb30a5f8
Finn <finn@uwave.fm>
2015-12-18 15:46:29 -0800

    Add celerymaker.py

---
+++ celerymaker.py
@@ -0,0 +1,21 @@
+"""Integrates celery and flask"""
+from celery import Celery
+
+
+def make_celery(app):
+    """Integrates celery and the flask app specified"""
+    celery = Celery(app.import_name,
+                    broker=app.config['CELERY_BROKER_URL'],
+                    backend=app.config['CELERY_RESULT_BACKEND'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
