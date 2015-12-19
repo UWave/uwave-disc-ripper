32b67e6b61786d2d238fe7a71bbc785cfb0bccab
Finn <finn@uwave.fm>
2015-12-18 17:02:30 -0800

    Update ripper.js to work better with this new async thing

--- static/ripper.js
+++ static/ripper.js
@@ -10,41 +10,58 @@ function makeButtons(i) {
   return buttons;
 }
 
-function updateList() {
-  fetch('/changer/status').then(function(response) {
-    response.json().then(function(slots) {
-      for(var i in slots) {
-        if(slots.hasOwnProperty(i)) {
-          var row = $("tr.slot_" + i);
-          if(row.length === 0) {
-            row = $("<tr>").addClass("slot_" + i)
-              .append($("<td>").addClass("slot_" + i).addClass("slot"))
-              .append($("<td>").addClass("slot_" + i).addClass("state"))
-              .append($("<td>").addClass("slot_" + i).addClass("album"))
-              .append($("<td>").addClass("slot_" + i).addClass("artist"))
-              .append(makeButtons(i));
-            $(".slots").append(row);
-          }
-          $(".slot.slot_" + i).text(i);
-          if(slots[i].hasOwnProperty("full")) {
-            if(slots[i].full) {
-              $(".state.slot_" + i).text("Full");
-              $(".buttons.slot_" + i + " .ejectOrLoad").removeClass("fa-arrow-circle-down").addClass("fa-eject");
-            } else {
-              $(".state.slot_" + i).text("Empty");
-              $(".buttons.slot_" + i + " .ejectOrLoad").removeClass("fa-eject").addClass("fa-arrow-circle-down");
-            }
-          }
-          if(slots[i].hasOwnProperty("album")) {
-            $(".album.slot_" + i).text(slots[i].album);
-          }
-          if(slots[i].hasOwnProperty("artist")) {
-            $(".artist.slot_" + i).text(slots[i].artist);
-          }
+function showChangerStatus(slots) {
+  for(var i in slots) {
+    if(slots.hasOwnProperty(i)) {
+      var row = $("tr.slot_" + i);
+      if(row.length === 0) {
+        row = $("<tr>").addClass("slot_" + i)
+          .append($("<td>").addClass("slot_" + i).addClass("slot"))
+          .append($("<td>").addClass("slot_" + i).addClass("state"))
+          .append($("<td>").addClass("slot_" + i).addClass("album"))
+          .append($("<td>").addClass("slot_" + i).addClass("artist"))
+          .append(makeButtons(i));
+        $(".slots").append(row);
+      }
+      $(".slot.slot_" + i).text(i);
+      if(slots[i].hasOwnProperty("full")) {
+        if(slots[i].full) {
+          $(".state.slot_" + i).text("Full");
+          $(".buttons.slot_" + i + " .ejectOrLoad").removeClass("fa-arrow-circle-down").addClass("fa-eject");
+        } else {
+          $(".state.slot_" + i).text("Empty");
+          $(".buttons.slot_" + i + " .ejectOrLoad").removeClass("fa-eject").addClass("fa-arrow-circle-down");
         }
+      }
+      if(slots[i].hasOwnProperty("album")) {
+        $(".album.slot_" + i).text(slots[i].album);
+      }
+      if(slots[i].hasOwnProperty("artist")) {
+        $(".artist.slot_" + i).text(slots[i].artist);
+      }
+    }
+  }
+}
+
+function waitFor(url, callback) {
+  fetch(url).then(function(response) {
+    response.json().then(function(result) {
+      if(result.state == "PENDING") {
+        setTimeout(waitFor, 500, url, callback);
+      } else {
+        callback(result);
       }
     });
   });
 }
 
-$(document).ready(updateList);
+function checkChangerStatus() {
+  fetch('/changer/status').then(function(response) {
+    response.json().then(function(info){
+      var url = info.updates;
+      waitFor(url, showChangerStatus);
+    });
+  });
+}
+
+$(document).ready(checkChangerStatus);
